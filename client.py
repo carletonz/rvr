002e8425c476d2f9389215b27451a262cc907e4a
carletonz <carletoz@uci.edu>
2022-09-11 22:50:30 -0700

    added a sleep, also stop motors when program exits

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import serial
 import constant as Constants
 from rvrClient import RVRClient
 from Sensor.SensorService import SensorService
+import time
 
 port = None
 lastResult = None
@@ -22,20 +23,25 @@ sensors.start()
 leftSpeed = 0
 rightSpeed = 0
 
+try:
+    while True:
+        rvr.writePacket(RVRClient.getWakeCommandPacket())
 
-while True:
-    rvr.writePacket(RVRClient.getWakeCommandPacket())
-
-    if leftSpeed == 100:
-        leftSpeed = 0
-        rightSpeed = 0
-        rvr.writePacket(RVRClient.getSetRawMotorsPacket(Constants.MODE_OFF, leftSpeed,
-                                                        Constants.MODE_OFF, rightSpeed,
-                                                        Constants.ST))
-    else:
-        rvr.writePacket(RVRClient.getSetRawMotorsPacket(Constants.MODE_FORWARD, leftSpeed,
-                                                        Constants.MODE_FORWARD, rightSpeed,
-                                                        Constants.ST))
-    leftSpeed += 1
-    rightSpeed += 1
-    rvr.readPackets(10)
+        if leftSpeed == 100:
+            leftSpeed = 0
+            rightSpeed = 0
+            rvr.writePacket(RVRClient.getSetRawMotorsPacket(Constants.MODE_OFF, leftSpeed,
+                                                            Constants.MODE_OFF, rightSpeed,
+                                                            Constants.ST))
+        else:
+            rvr.writePacket(RVRClient.getSetRawMotorsPacket(Constants.MODE_FORWARD, leftSpeed,
+                                                            Constants.MODE_FORWARD, rightSpeed,
+                                                            Constants.ST))
+        leftSpeed += 1
+        rightSpeed += 1
+        rvr.readPackets(10)
+        time.sleep(1)
+finally:
+    rvr.writePacket(RVRClient.getSetRawMotorsPacket(Constants.MODE_OFF, 0,
+                                                    Constants.MODE_OFF, 0,
+                                                    Constants.ST))
